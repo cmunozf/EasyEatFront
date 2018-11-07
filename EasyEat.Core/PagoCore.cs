5b59e47921d32dff9e7901ee61c14a667936d884
Cmunozf <cmunozf@eafit.edu.co>
2018-11-07 00:12:32 -0500

    Correcciones

--- EasyEat.Core/PagoCore.cs
+++ EasyEat.Core/PagoCore.cs
@@ -1,8 +1,11 @@
 using EasyEat.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 namespace EasyEat.Core
 {
@@ -27,7 +30,28 @@ namespace EasyEat.Core
 
         public void AddPago(PagoModel model)
         {
-            //DAL.PagoDataAccess.AddPago(model,user);
+            var baseAddress = "http://localhost:8090/pago/agregarpago";
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var http = (HttpWebRequest)WebRequest.Create(new Uri(baseAddress));
+            http.Accept = "application/json";
+            http.ContentType = "application/json";
+            http.Method = "POST";
+
+            string parsedContent = json;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] bytes = encoding.GetBytes(parsedContent);
+
+            Stream newStream = http.GetRequestStream();
+            newStream.Write(bytes, 0, bytes.Length);
+            newStream.Close();
+
+            var response = http.GetResponse();
+
+            var stream = response.GetResponseStream();
+            var sr = new StreamReader(stream);
+            var content = sr.ReadToEnd();
         }
 
         public void ModifyPago(PagoModel model)
